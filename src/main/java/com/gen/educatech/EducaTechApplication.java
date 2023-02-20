9286ad21ac2282e727715625b4bc418efc074734
Lucas Sant'Anna <98597613+LSantAnnaB@users.noreply.github.com>
2023-02-20 17:04:12 -0300

    Commit

--- src/main/java/com/gen/educatech/EducaTechApplication.java
+++ src/main/java/com/gen/educatech/EducaTechApplication.java
@@ -1,4 +1,4 @@
-package com.spring.educatech;
+package com.gen.educatech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/gen/educatech/repository/CategoryRepository.java
@@ -0,0 +1,20 @@
+package com.gen.educatech.repository;
+
+import com.gen.educatech.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+
+    public List<Category> findByTipoProdutoContainingIgnoreCase (@Param("tipo_produto") String tipoProduto);
+
+    public List<Category> findByDescricaoTipoProdutoContainingIgnoreCase (@Param("descricao_tipo_produto") String descricaoTipoProduto);
+
+    public List<Category> findByFornecedorContainingIgnoreCase (@Param("fornecedor") String fornecedor);
+    public Optional<Category> findById(Long id);
+}
